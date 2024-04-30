05d0fbd02915fc55f2eb7e7edbecf0989174cfb7
Dino_Back <alcendinoferreira,ity@gmail.com>
2024-04-30 01:48:28 -0300

    Anatomia das Classes

---
+++ java-anatomia-classes/README.md
@@ -0,0 +1,1 @@
+Anatomia das classes.
---
+++ java-anatomia-classes/src/edu/alcendino/sintaxejava/anatomiadasclasses/MinhaClasse.java
@@ -0,0 +1,17 @@
+package edu.alcendino.sintaxejava.anatomiadasclasses;
+public class MinhaClasse {
+
+public static void main (String [] args) {
+
+    String primeiroNome = "Alcendino";
+    String segundoNome = "Ferreira";
+
+    String nomeCompleto = nomeCompleto((primeiroNome), segundoNome);
+    System.out.println(nomeCompleto);
+    
+}
+public static String nomeCompleto(String primeiroNome, String segundoNome){
+    return primeiroNome.concat(" ").concat(segundoNome);
+}
+
+}
